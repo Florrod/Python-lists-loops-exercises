b0f0b81ee75fba68fdb053dcdee6544ee32300b2
Flor Rodriguez <florrod23@gmail.com>
2020-07-01 19:27:48 +0000

    Más ejercicios

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,11 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
+
+for i in range(0,10): # utilizamos un loop para repetir algo 10 veces en esta ocasión no para recorrer el array
+    my_list.append(random.randint(0,1000))
+
+print(my_list)
 
 #The magic is here:
--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -2,3 +2,18 @@ my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fern
 
 #Your code here:
 
+# my_sample_list[1] = "Steven"
+# my_sample_list.append("Pepe")
+# my_sample_list.insert(0,"Ruth"+"Pedro")
+# my_sample_list.remove("Annie")
+# my_sample_list.remove("Esmeralda")
+
+# for i in reversed(my_sample_list):
+#     print(i)
+
+my_sample_list[1] = "Steven"
+my_sample_list[-1]="Pepe"
+my_sample_list[0]= my_sample_list[2] + my_sample_list[4]
+
+for i in range(len(my_sample_list)-1,-1,-1):
+    print(my_sample_list[i])
